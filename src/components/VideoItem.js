e570ccc5da1d07c2587a812d46475fd14f77a075
Kyle Salstrom <kylesalstrom@gmail.com>
2019-12-17 16:46:18 -0800

    adds videolist, details, embedded videos & styling

---
+++ src/components/VideoItem.js
@@ -0,0 +1,24 @@
+import './VideoItem.css';
+import React from 'react';
+
+
+const VideoItem = ({ video, onVideoSelect}) => {
+    return (
+        <div className="item video-item" onClick={() => onVideoSelect(video)}>
+            <img
+                className="ui image"
+                alt={video.snippet.title}
+                src={video.snippet.thumbnails.medium.url} />
+            <div className="content">
+                <div className="header">
+                    {video.snippet.title}
+                </div>
+                {/* <div className="description">
+                    {video.snippet.description}
+                </div> */}
+            </div>
+        </div>
+    );
+};
+
+export default VideoItem;
