b200f1908535f414f49a9a6443ac6007bc154c67
Kyle Salstrom <kylesalstrom@gmail.com>
2022-06-13 21:42:16 -0700

    Update youtube.js

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyA2IvaYPB4Ji-pNsHYXLj_SvRL2ymeWaPY';
+const KEY = 'privateKey';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
